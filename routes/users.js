e17c890776000fa733b497127025b2fb10691dcc
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-03-21 13:36:18 +0530

    CRUD CODE

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,38 @@ router.get("/",(req, res)=>{
     res.send("home page chal rha hai")
 });
 
-router.get("/alluser",async (req, res)=>{
-    const allUser = await userModel.find();
-    res.send(allUser);
+// create
+// router.get("/user",async (req,res) =>{
+//     const createUser = await userModel.create({
+//         username: "joy",
+//         age: 22,
+//         name: "Joy"
+//     });
+//     res.send(createUser);
+// });
+
+// read all
+// router.get("/alluser",async (req, res)=>{
+//     const allUser = await userModel.find();
+//     res.send(allUser);
+// });
+
+//read one
+// router.get("/find",async (req, res)=>{
+//     const findOne = await userModel.findOne({
+//         username: "joy"
+//     });
+//     res.send(findOne);
+// });
+
+// delete
+router.get("/delete",async (req, res)=>{
+    const deleteUser = await userModel.findOneAndDelete({
+        username: "harsh"
+    });
+    res.send(deleteUser);
 });
+
 
 router.get("/profile",(req, res)=>{
     res.render("profile")
